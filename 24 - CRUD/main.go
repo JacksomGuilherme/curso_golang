0cf869271e5ab56b9c1100277cb33bc6d12a1a8d
JacksomGuilherme <jacksom.santos@autbank.com.br>
2025-07-08 15:00:13 -0300

    Commitando aulas sobre conexão com banco de dados e criação de um CRUD simples

---
+++ 24 - CRUD/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crud/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.AtualizarUsuario).Methods(http.MethodPut)
+	router.HandleFunc("/usuarios/{id}", servidor.DeletarUsuario).Methods(http.MethodDelete)
+
+	fmt.Println("Listening at: 127.0.0.1:5050")
+	log.Fatal(http.ListenAndServe(":5050", router))
+}
